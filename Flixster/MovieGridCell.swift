d68898af8d9e483024b54d239ecb89c92902ad21
Prinçia👑✨ <karen.tchemi@yahoo.com>
2019-02-05 02:11:11 -0500

    Flixster with collection view added

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Karen Tchemi on 2/5/19.
+//  Copyright © 2019 Karen Tchemi. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
